5ca06b5d8ba38a3e91d972e8cdcf95135e2834e0
shenioalves <shenioalvesjgs@gmail.com>
2025-06-04 17:25:40 -0300

    add client and client type models, client types page, and clients page

---
+++ lib/models/client.dart
@@ -0,0 +1,15 @@
+
+
+import 'client_type.dart';
+
+class Client {
+  String name;
+  String email;
+  ClientType type;
+
+  Client({
+    required this.name,
+    required this.email,
+    required this.type
+  });
+}
